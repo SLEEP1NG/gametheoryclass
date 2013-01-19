6dd2d4584a237ef238f6b3829a94595e133a96b4
Jeanne <nyjeanne@gmail.com>
2013-01-18 22:52:53 -0500

    add tests to check matrix of any size

--- src/main/groovy/jb/gametheory/algorithms/MixedStrategyNashEquilibrium.groovy
+++ src/main/groovy/jb/gametheory/algorithms/MixedStrategyNashEquilibrium.groovy
@@ -39,4 +39,66 @@ class MixedStrategyNashEquilibrium {
 		def q = (h-g) / qDenominator
 		return [[p, 1-p], [q, 1-q]]
 	}
+	
+	/**
+	 * Given table:
+	 * <pre>
+	 *      p     1-p
+	 * q    a,e   b,f
+	 * 1-q  c,g   d,h
+	 * </pre>
+	 * check:
+	 * a(player1[0]) + b(player1[1] + ... is same for each row
+	 * and totals for player 1 and 2 add up to 1 
+	 * 
+	 * @param matrix
+	 * @param player1Strategy
+	 * @param player2Strategy
+	 * @return
+	 */
+	def checkMixedStrategyNashEquilibrium(Object[][] matrix, Object[] player1Strategy, Object[] player2Strategy) {
+		return checkTotalIs100Percent(player1Strategy) && checkTotalIs100Percent(player2Strategy) && 
+			checkWeightedAverageForPlayer1(matrix, player1Strategy) && checkWeightedAverageForPlayer2(matrix, player2Strategy)
+	}
+	
+	def checkWeightedAverageForPlayer1(Object[][] matrix, Object[] strategy) {
+		def weighted = weightedAverageForRow(0, matrix[0], strategy)
+		for (i in 1 .. matrix.length-1) {
+			def current = weightedAverageForRow(0, matrix[i], strategy)
+			if (Math.abs(current - weighted) > 0.000001) {
+				return false;
+			}
+		}
+		return true
+	}
+	
+	def checkWeightedAverageForPlayer2(Object[][] matrix, Object[] strategy) {
+		Object[] temp = AlgorithmUtil.transformColumnCellsToArray(matrix, 0)
+		def weighted = weightedAverageForRow(1, temp, strategy)
+		for (i in 1 .. matrix.length-1) {
+			temp = AlgorithmUtil.transformColumnCellsToArray(matrix, i)
+			def current = weightedAverageForRow(1, temp, strategy)
+			if (Math.abs(current - weighted) > 0.000001) {
+				return false;
+			}
+		}
+		return true
+	}
+	
+	def weightedAverageForRow(int playerNum, Object[] row, Object[] strategy) {
+		def sum = 0
+		row.eachWithIndex{ it, i -> sum+= strategy[i] * it[playerNum]}
+		return sum
+	}
+	
+	def checkTotalIs100Percent(Object[] strategy) {
+		def sum = 0
+		strategy.each{  sum+= it}
+		return Math.abs(1 - sum) < 0.000001
+	}
+	
+
+	
+	// ------------------------------------------------------
+	
 }
--- src/test/groovy/jb/gametheory/algorithms/MixedStrategyNashEquilibriumTest.groovy
+++ src/test/groovy/jb/gametheory/algorithms/MixedStrategyNashEquilibriumTest.groovy
@@ -19,9 +19,11 @@ class MixedStrategyNashEquilibriumTest extends GroovyTestCase {
 					[[1, -1], [-1, 1]],
 					[[-1, 1], [1, -1]]
 				]
-		Object[] expected = [[0.5, 0.5], [0.5, 0.5]]
+		Object[] bothPlayersStrategy = [0.5, 0.5]
+		Object[] expected = [bothPlayersStrategy, bothPlayersStrategy]
 		Object[] actual = strategy.find2x2MixedStrategyNashEquilibrium(matrix)
 		assertArrayEquals(expected, actual)
+		assertTrue strategy.checkMixedStrategyNashEquilibrium(matrix, bothPlayersStrategy, bothPlayersStrategy)
 	}
 
 	// ----------------------------------------------------------------
@@ -32,9 +34,11 @@ class MixedStrategyNashEquilibriumTest extends GroovyTestCase {
 					[[1, 1], [0, 0]],
 					[[0, 0], [1, 1]]
 				]
-		Object[] expected = [[0.5, 0.5], [0.5, 0.5]]
+		Object[] bothPlayersStrategy = [0.5, 0.5]
+		Object[] expected = [bothPlayersStrategy, bothPlayersStrategy]
 		Object[] actual = strategy.find2x2MixedStrategyNashEquilibrium(matrix)
 		assertArrayEquals(expected, actual)
+		assertTrue strategy.checkMixedStrategyNashEquilibrium(matrix, bothPlayersStrategy, bothPlayersStrategy)
 	}
 
 	// ----------------------------------------------------------------
@@ -58,9 +62,13 @@ class MixedStrategyNashEquilibriumTest extends GroovyTestCase {
 					[[2, 1], [0, 0]],
 					[[0, 0], [1, 2]]
 				]
-		Object[] expected = [[1/3, 2/3], [2/3, 1/3]]
+		Object[] player1Strategy = [1/3, 2/3]
+		Object[] player2Strategy = [2/3, 1/3]
+		Object[] expected = [player1Strategy, player2Strategy]
 		Object[] actual = strategy.find2x2MixedStrategyNashEquilibrium(matrix)
 		assertArrayEquals(expected, actual)
+		assertTrue strategy.checkMixedStrategyNashEquilibrium(matrix, player1Strategy, player2Strategy)
+		assertFalse strategy.checkMixedStrategyNashEquilibrium(matrix, player2Strategy, player1Strategy)
 	}
 
 	// ----------------------------------------------------------------
@@ -71,9 +79,11 @@ class MixedStrategyNashEquilibriumTest extends GroovyTestCase {
 					[[0, 1], [1, 0]],
 					[[1, 0], [0, 1]]
 				]
-		Object[] expected = [[0.5, 0.5], [0.5, 0.5]]
+		Object[] bothPlayersStrategy = [0.5, 0.5]
+		Object[] expected = [bothPlayersStrategy, bothPlayersStrategy]
 		Object[] actual = strategy.find2x2MixedStrategyNashEquilibrium(matrix)
 		assertArrayEquals(expected, actual)
+		assertTrue strategy.checkMixedStrategyNashEquilibrium(matrix, bothPlayersStrategy, bothPlayersStrategy)
 	}
 
 	void testSoccer_weighted() {
@@ -82,12 +92,42 @@ class MixedStrategyNashEquilibriumTest extends GroovyTestCase {
 					[[0, 1], [1, 0]],
 					[[0.75, 0.25], [0, 1]]
 				]
-		Object[] expected = [
-			[0.5714285714, 0.4285714286],
-			[0.4285714286, 0.5714285714]
-		]
+		Object[] player1Strategy = [0.5714285714, 0.4285714286]
+		Object[] player2Strategy = [0.4285714286, 0.5714285714]
+		Object[] expected = [player1Strategy, player2Strategy]
 		Object[] actual = strategy.find2x2MixedStrategyNashEquilibrium(matrix)
 		assertArrayEquals(expected, actual)
+		assertTrue strategy.checkMixedStrategyNashEquilibrium(matrix, player1Strategy, player2Strategy)
+		assertFalse strategy.checkMixedStrategyNashEquilibrium(matrix, player2Strategy, player1Strategy)
+	}
+	
+	void testCheckerForWrongSums() {
+		Object[] good = [0.5, 0.5]
+		Object[] bad = [0.3, 0.5]
+		assertFalse "player1", strategy.checkMixedStrategyNashEquilibrium(null, bad, good)
+		assertFalse "player 2", strategy.checkMixedStrategyNashEquilibrium(null, good, bad)
+	}
+	
+	void testCheckerForWrongSolution() {
+		Object[][] matrix =
+				[
+					[[0, 1], [1, 0]],
+					[[1, 0], [0, 1]]
+				]
+		Object[] bothPlayersStrategy = [0.7, 0.3]
+		assertFalse strategy.checkMixedStrategyNashEquilibrium(matrix, bothPlayersStrategy, bothPlayersStrategy)
+	}
+	
+	void test_rockPaperSissors() {
+		Object[][] matrix =
+		[	/* xy yz xz */
+	/*x*/		[[0, 0], [-1,1], [1, -1]],
+	/*y*/		[[1, -1], [0, 0], [-1, 1]],
+	/*z*/		[[-1, 1], [1, -1], [0, 0]]
+		]
+		Object[] player1Strategy = [1/3, 1/3, 1/3]
+		Object[] player2Strategy = [1/3, 1/3, 1/3]
+		assertTrue strategy.checkMixedStrategyNashEquilibrium(matrix, player1Strategy, player2Strategy)
 	}
 
 }
